02a56007a63e2c7fbf27acd4f3ae04b8a458510f
algemorge <mafia18@naver.com>
2023-03-22 18:51:33 +0900

    프로필 중간점검0322

--- app.js
+++ app.js
@@ -6,11 +6,25 @@ app.set("view engine", "ejs");
 app.set("/views", "views");
 app.use("/static", express.static(__dirname + "/static"));
 app.get("/", (req, res) => {
-  res.render("profile_main");
+  res.render("common");
 });
 app.get("/contents", (req, res) => {
   res.render("contents")
 })
+app.get("/header", (req, res) => {
+  res.render("header")
+})
+app.get("/tagged", (req, res) => {
+  res.render("tagged")
+})
+app.get("/saved", (req, res) => {
+  res.render("saved")
+})
+app.get("/common/tagged", (req, res) => {
+  res.render("tagged")
+})
+
+
 /* 나중에 라우팅될 코드 get 요청 이용해서 작성 */
 // 포트 열기
 app.listen(PORT, () => {
