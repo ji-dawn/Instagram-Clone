a76c3990c702b20d223b83703f6a49af7082cb58
algemorge <mafia18@naver.com>
2023-03-24 19:52:09 +0900

    profile 완성

--- app.js
+++ app.js
@@ -32,6 +32,15 @@ app.get("/saved", (req, res) => {
 app.get("/common", (req, res) => {
   res.render("shin/common")
 })
+app.get("/clickp", (req, res) => {
+  res.render("shin/click-profile")
+})
+app.get("/follow", (req, res) => {
+  res.render("shin/follow")
+})
+app.get("/follower", (req, res) => {
+  res.render("shin/follower")
+})
 
 
 /* Bind and listen the connections on the specified host and port */
