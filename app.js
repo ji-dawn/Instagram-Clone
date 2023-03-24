0b1d0588ba6c90a70f3a1550fb36a5935d1f12f5
Jin <hellothisisjin@gmail.com>
2023-03-24 19:41:02 +0900

    feat : Like to story using req.params

--- app.js
+++ app.js
@@ -1,43 +1,47 @@
 /* Using Express */
-const express = require('express');
+const express = require("express");
 const app = express();
 
 /* Settings for port number */
 const PORT = 8000;
 
 /* Settings for port number */
-app.set('view engine', 'ejs');
-app.set('/views', 'views');
+app.set("view engine", "ejs");
+app.set("/views", "views");
 /* Settings for static directory */
-app.use('/static', express.static(__dirname + '/static'));
+app.use("/static", express.static(__dirname + "/static"));
 
 /* Routing */
-app.get('/', (req, res) => {
-  res.render('index'); /* Start path from /views/ */
+app.get("/", (req, res) => {
+  res.render("index"); /* Start path from /views/ */
 });
 
 /* Binding */
-app.get('/contents', (req, res) => {
-  res.render('shin/contents');
+app.get("/contents", (req, res) => {
+  res.render("shin/contents");
 });
-app.get('/header', (req, res) => {
-  res.render('shin/header');
+app.get("/header", (req, res) => {
+  res.render("shin/header");
 });
-app.get('/tagged', (req, res) => {
-  res.render('shin/tagged');
+app.get("/tagged", (req, res) => {
+  res.render("shin/tagged");
 });
-app.get('/saved', (req, res) => {
-  res.render('shin/saved');
+app.get("/saved", (req, res) => {
+  res.render("shin/saved");
 });
-app.get('/common', (req, res) => {
-  res.render('shin/common');
+app.get("/common", (req, res) => {
+  res.render("shin/common");
 });
-app.get('/main', (req, res) => {
-  res.render('main');
+app.get("/main", (req, res) => {
+  res.render("main");
 });
-app.get('/story', (req, res) => {
-  res.render('jihyo/story');
-});
+
+for (let i = 0; i < 8; ++i) {
+  app.get(`/story/:${i}`, (req, res, next) => {
+    console.log(req.params);
+    res.render("jihyo/story", { imageNum: req.params });
+  });
+}
 
 /* Bind and listen the connections on the specified host and port */
 app.listen(PORT, () => {
