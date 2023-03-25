e42523ec0026cfb11f3b58835dc17a98a719600c
마상위 <ma0719@naver.com>
2023-03-25 11:21:42 +0900

    comment 최종본

--- static/js/main.js
+++ static/js/main.js
@@ -5,16 +5,24 @@ const submitButton = document.querySelector(".btn span");
 function commentUpdate() {
   const ulbox = document.querySelector(".comments_info.comment_list_ul");
   const liBox = document.createElement("li");
-  const pBox = document.createElement("div");
+  const pBox1 = document.createElement("div");
+  const pBox2 = document.createElement("div");
   const userBox = document.createElement("span");
   const deleteIcon = document.createElement("i");
   const heartIcon = document.createElement('i');
   const commentCtnBox = document.createElement("span");
-  let user = "_SSANG";
-
+  let user = "PHOENIX";
+  
+  deleteIcon.classList.add('plusicon');
+  heartIcon.classList.add('plusicon');
+  
+  liBox.setAttribute('style','display:flex; justify-content:space-between');
+  pBox1.setAttribute('style','display:flex; align-items=center');
+  pBox2.setAttribute('style','display:flex; align-items=center');
   userBox.setAttribute("class", "user_id");
   commentCtnBox.setAttribute("class", "comment_contents");
-  pBox.setAttribute("class", "comments_tit");
+  pBox1.setAttribute("class", "comments_tit");
+  pBox2.setAttribute('class', "comments_tit");
   deleteIcon.setAttribute("class", "fa-regular fa-trash-can");
   heartIcon.setAttribute('class','fa-regular fa-heart');
   
@@ -23,11 +31,12 @@ function commentUpdate() {
   commentCtnBox.innerHTML = comments.value;
   
 
-  pBox.appendChild(userBox);
-  pBox.appendChild(commentCtnBox);
-  pBox.appendChild(heartIcon);
-  pBox.appendChild(deleteIcon);
-  liBox.appendChild(pBox);
+  pBox1.appendChild(userBox);
+  pBox1.appendChild(commentCtnBox);
+  pBox2.appendChild(heartIcon);
+  pBox2.appendChild(deleteIcon);
+  liBox.appendChild(pBox1);
+  liBox.appendChild(pBox2);
   ulbox.appendChild(liBox);
   comments.value = "";
 
@@ -55,20 +64,20 @@ let likenumber = document.querySelector(".likes_number");
 let user_id_img = document.querySelector('.user_id_img');
 let user_id_nick = document.querySelector('.user_id_nick');
 let profile_id = document.querySelector('.profile_id');
-let user1 = '__SSANG';
-let user2 = 'minchodan_31'
+let user1 = 'PHOENIX';
+let user2 = 'MONEY_31'
 heart.addEventListener("click", function () {
   if ($(heart).hasClass("fa-regular")) {
     heart.classList = "fa-solid fa-heart";
     heart.setAttribute("css", "color:red");
     heart.style.color = "red";
-    user_id_img.setAttribute('src','../static/src/sang/pexels-alexandra-folster-6307706.jpg');
+    user_id_img.setAttribute('src','../static/src/shin/bird.jpg');
     user_id_nick.innerText = user1;
     likenumber.innerText = parseInt(likenumber.innerText) + 1;
   } else {
     heart.classList = "fa-regular fa-heart";
     heart.style.color = "black";
-    user_id_img.setAttribute('src','../static/src/sang/mintchoco.jpeg');
+    user_id_img.setAttribute('src','../static/src/sang/money.jpg');
     user_id_nick.innerText = user2;
     // user1 = user_id_nick.innerText;
     likenumber.innerText = parseInt(likenumber.innerText) - 1;
